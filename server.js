8fd09ac97cd1d655d8733211ec5a26a9ec12cb6f
Vamsi0935 <dvkrishna142000@gmail.com>
2024-07-31 19:58:23 +0530

    changes

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 const app = express();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB connected...");
   })
