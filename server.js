0910f085ea430b43f0b226940117b632fc99e2b5
Vamsi0935 <dvkrishna142000@gmail.com>
2024-08-01 23:51:14 +0530

    changes

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://dvkrishna142000:lqiEq0L237tVduLp@mymongodb.ipgujet.mongodb.net/mongodb-ContactForm?retryWrites=true&w=majority&appName=mymongodb"
+  )
   .then(() => {
     console.log("MongoDB connected...");
   })
@@ -44,9 +47,14 @@ const Contact = mongoose.model("Contact", contactFormSchema);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ error: err.message });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/contact", async (req, res) => {
